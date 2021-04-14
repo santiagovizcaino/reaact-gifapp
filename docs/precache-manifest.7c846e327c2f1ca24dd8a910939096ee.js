0037aa0f44f7e30738ea6d980e447b7160c79ce4
Santiago Vizcaino <jvizcainot@est.ups.edu.ec>
2021-04-13 20:29:22 -0500

    docs agregado

---
+++ docs/precache-manifest.7c846e327c2f1ca24dd8a910939096ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a309153dae5bd79c37b3a00dab16a052",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e559bee23735951dab7",
+    "url": "/static/css/main.f04868c6.chunk.css"
+  },
+  {
+    "revision": "21128ddbf1ff63946978",
+    "url": "/static/js/2.31a10122.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.31a10122.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e559bee23735951dab7",
+    "url": "/static/js/main.8bded81f.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
